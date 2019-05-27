cf2122a80a5e07696c52190a53c6eef094329901
Rodrigo Moutinho <rcm1989@gmail.com>
2019-05-27 17:22:57 -0300

    outras variações para o uso de underscore #1

--- src/org/j6toj8/languageenhancements/literals/Literals_04_Underscore.java
+++ src/org/j6toj8/languageenhancements/literals/Literals_04_Underscore.java
@@ -9,12 +9,16 @@ public class Literals_04_Underscore {
     int i2 = 10_00_00_0; // o underscore pode estar em qualquer posição entre 2 números
     int i3 = _1000; // NÃO COMPILA - o underscore não pode estar no início
     int i4 = 1000_; // NÃO COMPILA - o underscore não pode estar no final
+    int i5 = 1___000; // COMPILA - vários underscore é permitido, desde que estejam entre 2 números
+    int i6 = 0x_100; // NÃO COMPILA - entre marcador de base não é permitido
+    int i7 = 0xF_F; // COMPILA - apesar de serem letras, representam valores numéricos dessa base
     
-    long l3 = 12_345_6_7890_123456_789L; // long com underscore
+    long l1 = 12_345_6_7890_123456_789L; // long com underscore
+    long l2 = 12_345_6_789_L; // NÃO COMPILA - não pode ficar ao lado de um marcador de tipo
     
-    double d2 = 100_000.01; // double com underscore
+    double d1 = 100_000.01; // double com underscore
     double d2 = 10_.01; // NÃO COMPILA - o underscore deve estar entre números
-    double d2 = 10._01; // NÃO COMPILA - o underscore deve estar entre números
+    double d3 = 10._01; // NÃO COMPILA - o underscore deve estar entre números
     // end::code[]
   }
 }

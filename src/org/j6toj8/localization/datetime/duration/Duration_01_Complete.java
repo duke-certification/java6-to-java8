f02c9ed7fcdd3d13d2ffeecc1fd29acd930e68e3
Rinaldo Pitzer Júnior <16694899+rinaldodev@users.noreply.github.com>
2019-06-03 19:34:19 -0300

    #10 ultima secao: Duration

---
+++ src/org/j6toj8/localization/datetime/duration/Duration_01_Complete.java
@@ -0,0 +1,10 @@
+package org.j6toj8.localization.datetime.duration;
+
+public class Duration_01_Complete {
+
+  // tag::code[]
+  public static void main(String[] args) {
+
+  }
+  // end::code[]
+}
